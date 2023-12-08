50f1eefa3cec4d5071741c8046e63e7cdb9f89ae
Francisco Kaique <kaique49nunes@gmail.com>
2023-12-08 15:49:41 -0300

    correcao

--- pc_trabalho03/Principal.java
+++ pc_trabalho03/Principal.java
@@ -8,8 +8,8 @@ public class Principal extends Application{
   * Matricula: 201610441
   * Inicio: 26/07/2018
   * Ultima alteracao: 29072018
-  * Nome: Nome do programa
-  * Funcao: descricao do que eh o programa
+  * Nome: Fork
+  * Funcao: Cria a arvore fork em JAVA
   *************************************************************** */
   public static void main(String[] args) {
         launch(args);
